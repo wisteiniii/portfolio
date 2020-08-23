7c12490b1d97083dc74fff6a2295bd1f7d04cb38
wisteiniii <williamjsteinmetziii@gmail.com>
2020-08-22 20:45:14 -0700

    Testing to deploy

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Navbar from "./Navbar/NavBar"
+import Navbar from "./Navbar/NavBar";
 
 function Header() {
   return (
-  <header>
-    <Navbar />
-  </header>)
+    <header>
+      <Navbar />
+    </header>
+  );
 }
 
 export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header";
-import MainContent from "../components/Body/MainContent";
+import MainContent from "../components/Rename/MainContent";
 import Footer from "../components/Footer/Footer";
 import "./App.css";
 
 class App extends Component {
-  render () {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+    );
   }
 }
 
